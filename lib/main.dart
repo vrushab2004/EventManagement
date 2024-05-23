13aac6a65b3b373d35602541c104ff2e868d2cb8
vrushab2004 <vrushabhsaa123@gmail.com>
2024-05-23 23:25:40 +0530

    connection to firebase done

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:event_mgm/HomePage.dart';
+import 'package:event_mgm/firebase_options.dart';
 import 'package:event_mgm/login1.dart';
 import 'package:event_mgm/login2.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 
-void main(){
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp( const MyApp());
 }
 class MyApp extends StatelessWidget {
